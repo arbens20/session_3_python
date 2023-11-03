d983b4e80cfa6fbf9eed937e7aea13cd0379b3d3
arbens20 <arbensvital@gmail.com>
2023-11-02 17:45:43 -0700

    first commit

---
+++ string_page.py
@@ -0,0 +1,37 @@
+
+
+
+#koupe chenn lan tout kote ki gen espace
+def cut_string():
+    phrase = input('Entrer la phrase : ')
+    result = phrase.split()
+    print (result)
+
+
+
+cut_string()
+
+
+# 2 mete premye let chak mo an majiskil
+
+# def premye_mo_majiskil():
+#     phrase = input('Entrer la phrase : ')
+#     result = ' '.join(word.capitalize)
+#     print(result)
+    
+    
+# rekipere premye let chak mo
+
+
+
+
+
+# Ranplase tout karakte "a" ak "@"
+
+def ranplase_let_a():
+    phrase = input('Entrer la phrase : ')
+    result = phrase.replace('a','@')
+    print(result)
+    
+    
+ranplase_let_a()
